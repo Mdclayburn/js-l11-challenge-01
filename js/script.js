732c9a4e8d47a81aa468051815691efd937e7173
mdclayburn <marisaclayburn17@gmail.com>
2021-05-25 15:47:58 -0600

    Programmed button to display random img

--- js/script.js
+++ js/script.js
@@ -7,5 +7,22 @@ const getImages = async function (){
     const res = await fetch("https://picsum.photos/v2/list?limit=100");
     const images = await res.json();
     console.log(res);
+    selectRandomImg(images);
 };
-getImages();
+
+const selectRandomImg = function(images){
+    const randomIndex = Math.floor(Math.random() * images.length);
+    const randomImg = images[randomIndex];
+    displayImg(randomImg);
+};
+
+const displayImg = function(randomImg){
+    const author = randomImg.author;
+    const imgAddress = randomImg.download_url;
+    authorSpan.innerText = author;
+    img.src= imgAddress;
+    imgDiv.classList.remove("hide");
+};
+button.addEventListener("click", function(){
+    getImages();
+});
